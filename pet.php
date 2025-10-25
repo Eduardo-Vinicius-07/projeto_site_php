63427171df520e72113e991c2ecb7ab4e435b98e
Eduardo Vinicius <eduardoviniii19@gmail.com>
2025-10-24 22:03:08 -0300

    PHP

--- pet.php
+++ pet.php
@@ -8,6 +8,34 @@ class Pet {
     public $porte;
     public $raca;
     public $foto;
+
+    public function _construct($conn , $nome, $sexo ,$porte, $raca ,$usuario_id, $foto= null){
+     $this->conn=$conn;
+     $this->nome-$nome;
+     $this->sexo=$sexo;
+     $this->porte=$porte;
+     $this->raca=$raca;
+     $this->foto=$foto;
+     $this->usuario_id=$usuario_id;
+     
+    }
+    public function salvar(){
+        $sql ="INSERT INTO pet(nome,sexo,porte,raca,foto) VALUES (?,?,?,?,?,?)";
+        $stmt = $this-> conn->prepare($sql);
+        if($stmt ->executer([$this->nome, $this->sexo, $this->porte, $this->raca,$this->usuario_id, $this->foto])){
+            $this->usuario_id = $this->lastInsertID();
+            return $this->usuario_id;
+     }  else{
+
+       throw new Exception ("Erro ao salva o Pet.");
+
+     }        
+
+        
+    }
+
+
+    }
 }
 
 ?>
